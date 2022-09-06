0542e8c03ecadb4d3595720f1e98f2645c963854
Walter Nuccio <walter.nuccio@agilelab.it>
2022-09-06 08:41:51 +0200

    modify first test title

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -3,7 +3,7 @@ import org.scalatest.matchers.should
 
 class Test extends AnyFlatSpec with should.Matchers {
 
-  "The test" should "do something" in {
+  "The test" should "succeed" in {
     1 shouldBe 1
   }
 
